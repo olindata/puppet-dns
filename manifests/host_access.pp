ff7f65e5b56b5ac3aced05fbe76aa64c93f61938
Walter Heck <walterheck@olindata.com>
2012-09-12 21:24:48 +0700

    Add support for allow and deny files

--- manifests/host_access.pp
+++ manifests/host_access.pp
@@ -1,12 +1,20 @@
 # Manages hosts.allow and hosts.deny files.
 # Right now, allows ssh access only from local
 # networks and specified jumpboxes.
-class dns::host_access ($hosts_access_file) {
+class dns::host_access ($hosts_allow_file, $hosts_deny_file) {
+  file { '/etc/hosts.allow' :
+      ensure => present,
+      mode   => 0644,
+      owner  => 'root',
+      group  => 'root',
+      source => $hosts_allow_file
+  }
+
   file { '/etc/hosts.deny':
       ensure  => present,
       mode    => 0644,
       owner   => 'root',
       group   => 'root',
-      content => template($hosts_access_file)
+      content => template($hosts_deny_file)
   }
 }
